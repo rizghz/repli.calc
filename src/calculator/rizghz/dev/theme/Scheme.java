d6391f287e02ec9351a8bd643b68e3b0b5737a2e
Ghøzali <riz.ghz@gmail.com>
2023-04-30 21:37:40 +0700

    Add files via upload

---
+++ src/calculator/rizghz/dev/theme/Scheme.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculator.rizghz.dev.theme;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author enigma
+ */
+public class Scheme {
+    
+    public static Color bg_1 = Color.decode("#20201F");
+    public static Color fg_1 = Color.decode("#FFFFFF");
+    public static Color fg_2 = Color.decode("#666666");
+    
+    public static Color blue_1 = Color.decode("#404E71");
+    public static Color blue_2 = Color.decode("#3C4968");
+    public static Color blue_3 = Color.decode("#394560");
+    
+    public static Color grey_1 = Color.decode("#3A3C39");
+    public static Color grey_2 = Color.decode("#313331");
+    public static Color grey_3 = Color.decode("#272827");
+    
+}
